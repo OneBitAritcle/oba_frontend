e923bc52898d1c523003b177dc47d18089c1d60e
Byunjihun <vkvlditm00@naver.com>
2025-12-02 21:18:04 +0900

    ON-66 전체 앱 레이아웃 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
-import "react-native-reanimated";
-
-import AppBackground from "./components/AppBackground";
-
-const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: "transparent",  // 🔥 Theme 기본 배경 투명화
-  },
-};
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <ThemeProvider value={MyTheme}>
-      <View style={{ flex: 1, backgroundColor: "transparent" }}>
-        <AppBackground />
-
-        <View style={{ flex: 1, backgroundColor: "transparent" }}>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              contentStyle: { backgroundColor: "transparent" }, // 유지
-            }}
-          />
-        </View>
-
-        <StatusBar style="auto" />
-      </View>
-    </ThemeProvider>
-  );
+  return <Slot />;
 }
