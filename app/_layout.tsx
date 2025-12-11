382a83c9b46f197af5e00c42b9683cf22aedcfa3
Thedduro <lsw2207@gmail.com>
2025-12-11 18:05:57 +0900

    expo app 실행 오류 해결

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,8 +6,9 @@ import "react-native-reanimated";
 import AppBackground from "./components/AppBackground";
 import { SplashScreen } from "expo-router";
 
-SplashScreen.preventAutoHideAsync();  // 디버그 UI 숨김용
-
+// SplashScreen.preventAutoHideAsync();  // 디버그 UI 숨김용 
+// 이 코드 때문에 앱에서 실행이 안됐음
+// 스플래시(초기 로딩 화면)를 수동 제어하려면 preventAutoHideAsync()와 hideAsync() 쌍이 필요함.
 
 const MyTheme = {
   ...DefaultTheme,
